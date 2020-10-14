e8f56a2031e4b2bd123023bb85b084c4aa286f6c
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-14 18:43:36 +0100

    added logout route and updated wording

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const userRoutes = express.Router();
 
-userRoutes.get('/', (req, res) => {
-  res.send('naa');
-});
-
 userRoutes.get('/success', (req, res) => {
   if (req.isAuthenticated()) {
-    res.send('welcome');
+    res.send('SUCCESFULLY LOGGED IN');
   } else {
     res.redirect('/api/user/failure');
   }
 });
 
 userRoutes.get('/failure', (req, res) => {
-  res.send('naa');
+  res.send('NOT AUTHENTICATED');
+});
+
+userRoutes.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = userRoutes;
