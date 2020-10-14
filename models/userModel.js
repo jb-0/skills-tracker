a33bc33fd606a057ac7c2161acf688b714648c82
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-14 18:43:08 +0100

    Updated user model for local logins

--- models/userModel.js
+++ models/userModel.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const findOrCreate = require('mongoose-findorcreate');
 
 const userSchema = new mongoose.Schema({
+  email: { type: String, required: false },
+  password: { type: String, required: false },
   googleId: { type: String, required: false },
   facebookId: { type: String, required: false },
   active: { type: Boolean, required: false, default: false },
