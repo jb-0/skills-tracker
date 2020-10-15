81f47d15c0106e3367f50e3b2ef7653ffc0de2f0
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-15 07:09:38 +0100

    Renamed routes and added a boolean isauthenticated route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -16,7 +16,7 @@ authRoutes.get(
   '/google/callback',
   passport.authenticate('google', { failureRedirect: '/api/user/failure' }),
   (req, res) => {
-    res.redirect('/api/user/success');
+    res.redirect('/api/user/loggedin');
   }
 );
 
@@ -27,33 +27,36 @@ authRoutes.get('/facebook', passport.authenticate('facebook'));
 
 authRoutes.get(
   '/facebook/callback',
-  passport.authenticate('facebook', { failureRedirect: '/api/user/failure' }),
+  passport.authenticate('facebook', { failureRedirect: '/api/user/loginfailure' }),
   (req, res) => {
-    res.redirect('/api/user/success');
+    res.redirect('/api/user/loggedin');
   }
 );
 
 /* *************************************************************************************************
 LOCAL ROUTES
+These are used in dev/test only and are disabled in production
 ************************************************************************************************* */
-authRoutes.post('/register', (req, res) => {
-  User.register(
-    new User({ email: req.body.email }),
-    req.body.password,
-    (err, user) => {
-      if (err) {
-        return res.send(`${err}\nUser: ${req.body.email}`);
+if (!process.env.PROD) {
+  authRoutes.post('/register', (req, res) => {
+    User.register(
+      new User({ email: req.body.email }),
+      req.body.password,
+      (err, user) => {
+        if (err) {
+          return res.status(401).send(`${err}`);
+        }
       }
+    );
+  });
+
+  authRoutes.post(
+    '/login',
+    passport.authenticate('local', { failureRedirect: '/api/user/loginfailure' }),
+    (req, res) => {
+      res.redirect('/api/user/loggedin');
     }
   );
-});
-
-authRoutes.post(
-  '/login',
-  passport.authenticate('local', { failureRedirect: '/api/user/failure' }),
-  (req, res) => {
-    res.redirect('/api/user/success');
-  }
-);
+}
 
 module.exports = authRoutes;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,21 +2,29 @@ const express = require('express');
 
 const userRoutes = express.Router();
 
-userRoutes.get('/success', (req, res) => {
+userRoutes.get('/loggedin', (req, res) => {
   if (req.isAuthenticated()) {
-    res.send(`SUCCESFULLY LOGGED IN: ${req.user}`);
+    res.send(`Successfully logged in: ${req.user}`);
   } else {
     res.redirect('/api/user/failure');
   }
 });
 
-userRoutes.get('/failure', (req, res) => {
-  res.send('NOT AUTHENTICATED');
+userRoutes.get('/loginfailure', (req, res) => {
+  res.status(401).send('Authentication unsuccessful');
 });
 
 userRoutes.get('/logout', (req, res) => {
   req.logout();
   res.redirect('/');
+});
+
+userRoutes.get('/isloggedin', (req, res) => {
+  if (req.isAuthenticated()) {
+    res.json(true);
+  } else {
+    res.json(false);
+  }
 });
 
 module.exports = userRoutes;
