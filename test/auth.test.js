24f0d8580dec04b13646543eb48ca6cb8f959a2a
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-15 08:10:13 +0100

    Added tests for login

--- test/auth.test.js
+++ test/auth.test.js
@@ -18,6 +18,9 @@ describe('User routes', function () {
     });
   });
 
+  // *************************************
+  // REGISTER
+  // *************************************
   describe('POST /auth/register', function () {
     it('it is not possible to locally register a user without a password', async function () {
       const res = await request(app).post('/auth/register').send({
@@ -65,6 +68,51 @@ describe('User routes', function () {
         res.text,
         'UserExistsError: A user with the given username is already registered'
       );
+    });
+  });
+
+  // *************************************
+  // LOGIN
+  // *************************************
+  describe('POST /auth/login', function () {
+    it('it is not possible to login locally without a password', async function () {
+      const res = await request(app).post('/auth/login').send({
+        email: process.env.TEST_USER,
+        password: undefined,
+      });
+
+      assert.strictEqual(res.status, 302);
+      assert.strictEqual(res.header.location, '/api/user/loginfailure');
+    });
+
+    it('it is not possible to login locally without an email', async function () {
+      const res = await request(app).post('/auth/login').send({
+        email: undefined,
+        password: process.env.TEST_USER_PASSWORD,
+      });
+
+      assert.strictEqual(res.status, 302);
+      assert.strictEqual(res.header.location, '/api/user/loginfailure');
+    });
+
+    it('it is not possible to login locally with an invalid password', async function () {
+      const res = await request(app).post('/auth/login').send({
+        email: process.env.TEST_USER,
+        password: 'CompletelyWrong',
+      });
+
+      assert.strictEqual(res.status, 302);
+      assert.strictEqual(res.header.location, '/api/user/loginfailure');
+    });
+
+    it('a user can login when valid email and password are supplied', async function () {
+      const res = await request(app).post('/auth/login').send({
+        email: process.env.TEST_USER,
+        password: process.env.TEST_USER_PASSWORD,
+      });
+
+      assert.strictEqual(res.status, 302);
+      assert.strictEqual(res.header.location, '/api/user/loggedin');
     });
   });
 });
