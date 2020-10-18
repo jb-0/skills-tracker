86ef897857f82e1b1e23aef8a6dee5af27eb8979
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-18 11:24:50 +0100

    added test case covering removal of keywords that are not permitted

--- test/job.test.js
+++ test/job.test.js
@@ -11,35 +11,52 @@ const { prepareQuery, searchReed } = require('../services/jobServices');
 // JOB SERVICES TESTS
 // *************************************
 describe('Job Services', function () {
-  it('locations that are not permitted are replaced with london', function () {
-    const testCases = ['llondon', '[=?{]\'/!{"`'];
-    const expectedQuery = 'keywords=react%20sql%20&locationName=london&distanceFromLocation=10';
+  describe('prepareQuery()', function () {
+    it('locations that are not permitted are replaced with london', function () {
+      const testCases = ['llondon', '[=?{]\'/!{"`'];
+      const expectedQuery = 'keywords=react%20sql&locationName=london&distanceFromLocation=10';
 
-    testCases.forEach((testCase) => {
-      const preparedQuery = prepareQuery({
-        distanceFromLocation: 10,
-        keywords: 'react sql',
-        locationName: testCase,
+      testCases.forEach((testCase) => {
+        const preparedQuery = prepareQuery({
+          distanceFromLocation: 10,
+          keywords: 'react sql',
+          locationName: testCase,
+        });
+
+        assert.strictEqual(preparedQuery, expectedQuery);
       });
-
-      assert.strictEqual(preparedQuery, expectedQuery);
     });
-  });
 
-  it('permitted locations are not replaced with london', function () {
-    const testCases = ['sheffield', 'devon'];
+    it('permitted locations are not replaced with london', function () {
+      const testCases = ['sheffield', 'devon'];
 
-    testCases.forEach((testCase) => {
-      const expectedQuery = `keywords=react%20sql%20&locationName=${
-        testCase}&distanceFromLocation=10`;
+      testCases.forEach((testCase) => {
+        const expectedQuery = `keywords=react%20sql&locationName=${
+          testCase}&distanceFromLocation=10`;
 
-      const preparedQuery = prepareQuery({
-        distanceFromLocation: 10,
-        keywords: 'react sql',
-        locationName: testCase,
+        const preparedQuery = prepareQuery({
+          distanceFromLocation: 10,
+          keywords: 'react sql',
+          locationName: testCase,
+        });
+
+        assert.strictEqual(preparedQuery, expectedQuery);
       });
+    });
 
-      assert.strictEqual(preparedQuery, expectedQuery);
+    it('keywords that are not permitted are removed', function () {
+      const testCases = ['react farming sql', 'react sql flying [=?{]\'/!{"` as[=?{]\'/!{"`a'];
+      const expectedQuery = 'keywords=react%20sql&locationName=london&distanceFromLocation=10';
+
+      testCases.forEach((testCase) => {
+        const preparedQuery = prepareQuery({
+          distanceFromLocation: 10,
+          keywords: testCase,
+          locationName: 'london',
+        });
+
+        assert.strictEqual(preparedQuery, expectedQuery);
+      });
     });
   });
 });
