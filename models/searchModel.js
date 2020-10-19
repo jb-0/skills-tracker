72d87a392063cbac8c14a5cf1133525374f21642
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-19 08:02:47 +0100

    Renamed saved search model to search

--- models/searchModel.js
+++ models/searchModel.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 /**
  * Schema is used to save search parameters. Also contains daily count of no. of jobs on reed
  */
-const savedSearchSchema = new mongoose.Schema({
+const searchSchema = new mongoose.Schema({
   searchTerms: { type: Object, required: true },
   dailySearchTermCount: { type: Array, required: false },
 });
 
-const SavedSearch = mongoose.model('SavedSearch', savedSearchSchema);
+const Search = mongoose.model('SavedSearch', searchSchema);
 
-module.exports = { SavedSearch };
+module.exports = { Search };
