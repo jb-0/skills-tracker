aaff1401dee69c5cf62bca85641078ab56e0a8da
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-19 07:54:55 +0100

    Updated searchTerms to object from array

--- models/savedSearchModel.js
+++ models/savedSearchModel.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 /**
- * Schema is used to save search term combinations. Also contains daily count of no. of jobs on reed
+ * Schema is used to save search parameters. Also contains daily count of no. of jobs on reed
  */
 const savedSearchSchema = new mongoose.Schema({
-  searchTerms: { type: Array, required: true },
+  searchTerms: { type: Object, required: true },
   dailySearchTermCount: { type: Array, required: false },
 });
 
