ae83d455fd70a29c3d33ad06e1e3d1af6b1a33ae
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-22 07:13:54 +0100

    Updated to use middleware auth check

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const jobRoutes = express.Router();
 const { searchReed, saveSearch, deleteUserSavedSearch } = require('../services/jobServices');
+const isLoggedIn = require('../middleware/isLoggedIn');
 
 // GET Jobs, using provided search terms.
 jobRoutes.get('/search', async (req, res) => {
@@ -11,20 +12,16 @@ jobRoutes.get('/search', async (req, res) => {
 });
 
 // POST Save search
-jobRoutes.post('/search/save', async (req, res) => {
-  if (req.isAuthenticated()) {
-    const result = await saveSearch(req);
-    res.status(result.code).send(result.msg);
-  } else {
-    res.redirect('/api/user/loginfailure');
-  }
+jobRoutes.post('/search/save', isLoggedIn, async (req, res) => {
+  const result = await saveSearch(req);
+  res.status(result.code).send(result.msg);
 });
 
 // PATCH Edit saved search, will delete trend history
 jobRoutes.patch('/search/edit/:id', (req, res) => {});
 
 // DELETE Saved search
-jobRoutes.delete('/search/delete/:id', async (req, res) => {
+jobRoutes.delete('/search/delete/:id', isLoggedIn, async (req, res) => {
   const result = await deleteUserSavedSearch(req);
   res.status(result.code).send(result.msg);
 });
