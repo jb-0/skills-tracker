510239097bcb9371caab56f0e645905f5d58ca13
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-24 13:35:35 +0100

    Created home page with hero image using the ImageContainer component

---
+++ client/src/components/home/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ImageContainer from '../common/ImageContainer';
+
+function HomePage() {
+  return (
+    <div className="home-top-container">
+      <ImageContainer src="/images/home-hero.png" alt="Laptop and plant hero" />
+    </div>
+  );
+}
+
+export default HomePage;
