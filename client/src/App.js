23f7e0002a074211636de0396806e1d54589fd62
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-24 13:35:49 +0100

    Include home page in App

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { ViewProvider } from './context/ViewContext'
 import Navbar from './components/common/Navbar';
+import HomePage from './components/home/HomePage'
 
 function App() {
   return (
     <div className="app">
       <ViewProvider>
         <Navbar />
+        <HomePage />
       </ViewProvider>
     </div>
   );
