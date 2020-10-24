212d10180ad3ae4e1d75de70356d8d1f3df39019
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-24 16:05:44 +0100

    Switched from using Search to SearchContainer

--- client/src/components/home/HomePage.jsx
+++ client/src/components/home/HomePage.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import ImageContainer from '../common/ImageContainer';
 import './HomePage.css'
+import Image from '../common/Image';
+import SearchContainer from '../common/SearchContainer'
+
 
 function HomePage() {
   return (
     <div className="home-top-container">
-      <ImageContainer src="/images/home-hero.png" alt="Laptop and plant hero" />
+      <Image src="/images/home-hero.png" alt="Laptop and plant hero" />
       <div className="overlay-image-container">
         <h1>Search for your skillset</h1>
+        <SearchContainer />
       </div>
       
     </div>
