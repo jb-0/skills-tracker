6d7defe25514d9e4446fd79eb7aa9c48a5b4093e
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-24 15:25:26 +0100

    Added styling to overlay image, added heading

--- client/src/components/home/HomePage.jsx
+++ client/src/components/home/HomePage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ImageContainer from '../common/ImageContainer';
+import './HomePage.css'
 
 function HomePage() {
   return (
     <div className="home-top-container">
       <ImageContainer src="/images/home-hero.png" alt="Laptop and plant hero" />
+      <div className="overlay-image-container">
+        <h1>Search for your skillset</h1>
+      </div>
+      
     </div>
   );
 }
