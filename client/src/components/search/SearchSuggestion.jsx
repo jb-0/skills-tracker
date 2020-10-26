739cf6edf0f6e2ad989da018e9eef92d85dd22ac
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-26 07:32:28 +0000

    Added key to generated suggested terms

--- client/src/components/search/SearchSuggestion.jsx
+++ client/src/components/search/SearchSuggestion.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './SearchSuggestion.css';
+import { v4 as uuidv4 } from 'uuid';
 
 function SearchSuggestion(props) {
   return (
@@ -10,6 +11,7 @@ function SearchSuggestion(props) {
             onClick={props.addSearchTerm}
             id={suggestedTerm}
             className="suggested-term-container"
+            key={uuidv4()}
           >
             <p id={suggestedTerm}>{suggestedTerm}</p>
           </div>
