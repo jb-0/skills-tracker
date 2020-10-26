f0d691acfddfbbf4939767eca4cdb9610c190673
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-26 07:22:05 +0000

    Added additional tests (including snapshots) for the search terms component

--- client/src/components/search/SearchTerms.test.js
+++ client/src/components/search/SearchTerms.test.js
@@ -2,6 +2,7 @@ import '@testing-library/jest-dom/extend-expect';
 import React from "react";
 import { unmountComponentAtNode } from "react-dom";
 import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import { act } from "react-dom/test-utils";
 
 import SearchTerms from "./SearchTerms";
@@ -21,7 +22,7 @@ describe('SearchTerms component', () => {
     container = null;
   });
 
-  it("terms passed in via array are rendered successfully", () => {
+  it('terms passed in via array are rendered successfully and prefixed with "x"', () => {
     act(() => {
       render(<SearchTerms searchTerms={['node', 'sql']} />, container);
     });
@@ -29,4 +30,26 @@ describe('SearchTerms component', () => {
     expect(screen.getByText('x node')).toBeInTheDocument();
     expect(screen.getByText('x sql')).toBeInTheDocument();
   });
+
+  it('when an empty array is passed, no terms (prefixed with "x") are rendered', () => {
+    act(() => {
+      render(<SearchTerms searchTerms={[]} />, container);
+    });
+
+    expect(screen.queryByText('x')).toBeNull();
+  });
+
+  it('renders correctly when passed an array containing search terms', () => {
+    const tree = renderer
+      .create(<SearchTerms searchTerms={['node', 'sql']} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly when passed an empty array', () => {
+    const tree = renderer
+      .create(<SearchTerms searchTerms={[]} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });  
 });
