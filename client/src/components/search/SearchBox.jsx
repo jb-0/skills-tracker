9298329177feef6ffce5fc3871f20f7c2cb2c5c1
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-27 20:28:43 +0000

    Refactored code to allow it to focus on search box, other components moved to SearchContainer

--- client/src/components/search/SearchBox.jsx
+++ client/src/components/search/SearchBox.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import './Search.css';
+import './SearchBox.css';
 import './SearchSuggestion';
-import SearchSuggestion from './SearchSuggestion';
-import SearchTerms from './SearchTerms'
-import { v4 as uuidv4 } from 'uuid';
 
 function Search(props) {
   return (
@@ -16,17 +13,6 @@ function Search(props) {
         value={props.searchInputText}
         autoComplete="off"
       ></input>
-
-      <SearchSuggestion
-        suggestedTerms={props.suggestedTerms}
-        addSearchTerm={props.addSearchTerm}
-      />
-
-      <SearchTerms
-        searchTerms={props.searchTerms}
-        removeSearchTerm={props.removeSearchTerm}
-        key={uuidv4()}
-      />
     </div>
   );
 }
