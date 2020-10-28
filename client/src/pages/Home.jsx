214cbf87e6d14a51d71883e02795069c2625da92
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-28 06:08:00 +0000

    Made home page header responsive

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Home.css'
 import Image from '../components/common/Image';
 import SearchContainer from '../components/search/SearchContainer'
-
+import { ViewContext } from '../context/ViewContext';
 
 function HomePage() {
+  const size = useContext(ViewContext);
   return (
-    <div className="home-top-container">
+    <div className={
+      size.device === 'Desktop'
+        ? 'home-top-container'
+        : 'home-top-container-mobile home-top-container'
+    }>
       <Image src="/images/home-hero.png" alt="Laptop and plant hero" />
       <div className="overlay-image-container">
         <h1>Search for your skillset</h1>
