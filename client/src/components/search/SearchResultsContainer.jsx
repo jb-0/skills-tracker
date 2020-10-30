f6bd2cebc8a4ba75d6bae3282d37912ba908895d
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-30 16:48:56 +0000

    Moved fetchdata into useEffect to address linter warning regarding fetchdata being an unreferenced dependency

--- client/src/components/search/SearchResultsContainer.jsx
+++ client/src/components/search/SearchResultsContainer.jsx
@@ -4,33 +4,33 @@ import SearchResults from './SearchResults';
 function SearchResultsContainer(props) {
   const [jobs, setJobs] = useState([]);
 
-  async function fetchData() {
-    const searchTermString = props.searchTerms.join(' ');
-
-    let apiQuery = `/api/job/search?keywords=${searchTermString}`;
-    apiQuery += `&locationName=${props.location}`;
-    apiQuery += `&distanceFromLocation=10`;
-
-    const res = await fetch(
-      apiQuery,
-      {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          frontend: 'react-frontend',
-        },
-      }
-    );
-    
-    if (res.status === 200) {
-      setJobs(await res.json());
-    } else {
-      setJobs({ noOfResults: 0, msg: 'no results found' });
-    }
-  }
-
   useEffect(() => {
+    async function fetchData() {
+      const searchTermString = props.searchTerms.join(' ');
+  
+      let apiQuery = `/api/job/search?keywords=${searchTermString}`;
+      apiQuery += `&locationName=${props.location}`;
+      apiQuery += `&distanceFromLocation=10`;
+  
+      const res = await fetch(
+        apiQuery,
+        {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            frontend: 'react-frontend',
+          },
+        }
+      );
+      
+      if (res.status === 200) {
+        setJobs(await res.json());
+      } else {
+        setJobs({ noOfResults: 0, msg: 'no results found' });
+      }
+    }
+    
     fetchData();
   }, [props.searchTerms, props.location]);
 
