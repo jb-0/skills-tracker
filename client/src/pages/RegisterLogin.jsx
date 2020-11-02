98835011da3b53c5c837b211fd73fb1940c3a6ba
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-02 07:20:12 +0000

    Implemented a simpler approach for FB logins, need to establish a better redirect

--- client/src/pages/RegisterLogin.jsx
+++ client/src/pages/RegisterLogin.jsx
@@ -3,10 +3,15 @@ import './RegisterLogin.css'
 import Button from '../components/common/Button'
 
 function RegisterLogin() {
+  async function facebookLogin() {
+    window.open("http://localhost:4000/auth/facebook", "_self");
+  }
+
+
   return (
     <div>
-      <Button buttonText='Login with facebook' classNames='social-button' buttonAction='' />
-      <Button buttonText='Login with google' classNames='social-button' buttonAction='' />
+      <Button buttonText='Login with facebook' classNames='social-button' buttonAction={facebookLogin} />
+      {/* <Button buttonText='Login with google' classNames='social-button' buttonAction='' /> */}
     </div>
   );
 }
