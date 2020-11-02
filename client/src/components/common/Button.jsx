5201ce89503d7ad291665e0df45f5b182e049041
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-02 07:02:54 +0000

    Enhanced button component to allow additional classnames to be passed as a prop

--- client/src/components/common/Button.jsx
+++ client/src/components/common/Button.jsx
@@ -3,7 +3,7 @@ import './Button.css';
 
 function Button(props) {
   return (
-    <button className="base-button" onClick={props.buttonAction} type="button">
+    <button className={"base-button " + props.classNames} onClick={props.buttonAction} type="button">
       {props.buttonText}
     </button>
   );
