c941295b9807131cf5c79976612cefe309e57ffe
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-02 07:03:13 +0000

    Added react router and implemented routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,28 @@ import './App.css';
 import { ViewProvider } from './context/ViewContext'
 import Navbar from './components/common/Navbar';
 import Home from './pages/Home'
+import RegisterLogin from './pages/RegisterLogin'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
       <ViewProvider>
         <Navbar />
-        <Home />
+          <Router>
+            <Switch>
+              <Route path="/login">
+                <RegisterLogin />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </Router>
       </ViewProvider>
     </div>
   );
