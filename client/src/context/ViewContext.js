074139387231835e99e86a3a1b0b2b59bc57203e
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-04 17:07:42 +0000

    Added a comment detailing the purpose of this context

--- client/src/context/ViewContext.js
+++ client/src/context/ViewContext.js
@@ -1,3 +1,8 @@
+/**
+  * Being able to get the screen dimensions in any component is useful for conditionally rendering
+  * based on device type as well as changing class names for styling purpose. This context is used
+  * to provide this dimension information as well as classify whether the width is mobile or desktop
+  */
 import React, { createContext, useState, useLayoutEffect } from 'react';
 
 export const ViewContext = createContext({
