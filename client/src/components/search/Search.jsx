67f952e0e3a69dde34f372686594151419625067
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-05 20:15:03 +0000

    Added some commentary and spacing between rendered elements for clarity

--- client/src/components/search/Search.jsx
+++ client/src/components/search/Search.jsx
@@ -18,19 +18,31 @@ function SearchContainer() {
     <div>
       {/* While props are used for common components, search functionality uses the SearchContext 
       and as a result no props are passed down */}
+      {/* The dropdown selection allows the user to choose a city/location */}
       <DropdownSelect
         name="location"
         selectOptions={permittedLocations}
         value={search.location}
         onChange={search.handleDropDownSelectUpdates}
       />
+
+      {/* The search box allows users to start typing a skill */}
       <SearchBox />
+
+      {/* Search suggestions are rendered as the user starts typing in the search box */}
       <SearchSuggestion />
+
+      {/* The search button triggers a fetch to the backend to return the number of jobs */}
       <Button
         buttonText="Search"
         buttonAction={search.handleSearchButtonClick}
       />
+
+      {/* When a suggestion is clicked it is rendered as a selected search term  */}
       <SearchTerms />
+
+      {/* As long as the user has select at least one search term and pressed search, the search
+      results will be returned and displayed */}
       {search.submittedSearchTerms.length > 0 ? (
         <SearchResultsContainer />
       ) : null}
