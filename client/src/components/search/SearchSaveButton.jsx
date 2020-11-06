c654ddaa2bc0b93b8d67b82568ddd2c0f3dbe594
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-06 21:05:33 +0000

    Added  call to back end to save search for authenticated users

--- client/src/components/search/SearchSaveButton.jsx
+++ client/src/components/search/SearchSaveButton.jsx
@@ -7,10 +7,47 @@ function SearchSaveButton() {
   const [userState] = useContext(UserContext)
   const [search]  = useContext(SearchContext);
 
+  async function saveSearch() {
+    const searchTermString = search.searchTerms.join(' ');
+
+    const body = {
+      keywords: searchTermString,
+      locationName: search.location,
+      distanceFromLocation: '10'
+    }
+
+    const res = await fetch(
+      '/api/job/search/save',
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          frontend: 'react-frontend',
+        },
+        body: JSON.stringify(body)
+      }
+    );
+    
+    try {
+      const json = await res.json();
+      console.log('json', json)
+    } catch (err) {
+      console.error('err', err);
+    }
+  
+    
+    // if (res.status === 200) {
+    //   setJobs(await res.json());
+    // } else {
+    //   setJobs({ noOfResults: 0, msg: 'no results found' });
+    // }
+  }
+
   return (
     <div>
       {search.submittedSearchTerms.length > 0 && userState.authenticated ? (
-        <Button buttonText="Save" buttonAction={() => {}} />
+        <Button buttonText="Save" buttonAction={saveSearch} />
       ) : null}
     </div>
   );
