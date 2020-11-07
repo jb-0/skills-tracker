f8388a0848af1322fc5a3d46267938ad7e528372
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-07 13:23:36 +0000

    Created an alert which is conditionally rendered informing user whether save was succesful or otherwise

--- client/src/components/search/SearchSaveButton.jsx
+++ client/src/components/search/SearchSaveButton.jsx
@@ -1,11 +1,21 @@
-import React,{ useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { SearchContext } from '../../context/SearchContext';
 import { UserContext } from '../../context/UserContext';
 import Button from '../common/Button';
+import Alert from '@material-ui/lab/Alert';
 
 function SearchSaveButton() {
-  const [userState] = useContext(UserContext)
-  const [search]  = useContext(SearchContext);
+  const [userState] = useContext(UserContext);
+  const [search] = useContext(SearchContext);
+
+
+  const alertDefaults = {
+    msg: undefined,
+    severity: undefined,
+    visible: false,
+  };
+
+  const [alert, setAlert] = useState(alertDefaults);
 
   async function saveSearch() {
     const searchTermString = search.searchTerms.join(' ');
@@ -13,41 +23,58 @@ function SearchSaveButton() {
     const body = {
       keywords: searchTermString,
       locationName: search.location,
-      distanceFromLocation: '10'
-    }
+      distanceFromLocation: '10',
+    };
 
-    const res = await fetch(
-      '/api/job/search/save',
-      {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          frontend: 'react-frontend',
-        },
-        body: JSON.stringify(body)
-      }
-    );
-    
+    const res = await fetch('/api/job/search/save', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        frontend: 'react-frontend',
+      },
+      body: JSON.stringify(body),
+    });
+
     try {
-      const json = await res.json();
-      console.log('json', json)
+      const response = await res.json();
+      const alertValues = {visible: true, msg: response.msg};
+
+      switch(res.status) {
+        case 200:
+          alertValues['severity'] = 'success';
+          break;
+        case 409:
+          alertValues['severity'] = 'warning';
+          break;
+        default:
+          throw new Error('unexpected response code');
+      }
+      
+      setAlert(alertValues);
+
     } catch (err) {
-      console.error('err', err);
+      setAlert({
+        msg: 'an unexpected error has occurred, please try again',
+        severity: 'error',
+        visible: true,
+      });
     }
-  
-    
-    // if (res.status === 200) {
-    //   setJobs(await res.json());
-    // } else {
-    //   setJobs({ noOfResults: 0, msg: 'no results found' });
-    // }
+  }
+
+  function handleAlertClose() {
+    setAlert(alertDefaults);
   }
 
   return (
     <div>
       {search.submittedSearchTerms.length > 0 && userState.authenticated ? (
         <Button buttonText="Save" buttonAction={saveSearch} />
+      ) : null}
+      {alert.visible ? (
+        <Alert severity={alert.severity} onClose={handleAlertClose}>
+          {alert.msg}
+        </Alert>
       ) : null}
     </div>
   );
