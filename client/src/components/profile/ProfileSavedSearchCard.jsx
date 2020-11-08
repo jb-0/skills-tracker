41f5ddca326f0f5f74ba8b9a4f18054fcae486ae
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-08 18:45:28 +0000

    Added charts.js and added boiler plate example to each saved search card to get a feel for how it will look (looks great!)

--- client/src/components/profile/ProfileSavedSearchCard.jsx
+++ client/src/components/profile/ProfileSavedSearchCard.jsx
@@ -1,7 +1,45 @@
-import React from 'react';
-import './ProfileSavedSearchCard.css'
+import React, { useEffect } from 'react';
+import './ProfileSavedSearchCard.css';
+import Chart from 'chart.js';
 
 function ProfileSavedSearchCard(props) {
+
+  useEffect(() => {
+    async function createChart() {
+      const ctx = document.getElementById(props.search._id).getContext('2d');
+      const chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
+        data: {
+          labels: [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+          ],
+          datasets: [
+            {
+              label: 'My First dataset',
+              backgroundColor: 'rgb(255, 99, 132)',
+              borderColor: 'rgb(255, 99, 132)',
+              data: [0, 10, 5, 2, 20, 30, 45],
+            },
+          ],
+        },
+
+        // Configuration options go here
+        options: {},
+      });
+    }
+
+    createChart();
+  }, []);
+
   return (
     <div className="saved-search-card">
       <p>
@@ -9,6 +47,7 @@ function ProfileSavedSearchCard(props) {
         <br />
         Location: {props.search.searchTerms.locationName}
       </p>
+      <canvas id={props.search._id}></canvas>
     </div>
   );
 }
