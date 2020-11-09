95d7cdb401e16a98625c4bb62a68b50adff89a73
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-09 06:18:35 +0000

    Updated login links for prod

--- client/src/pages/RegisterLogin.jsx
+++ client/src/pages/RegisterLogin.jsx
@@ -5,14 +5,14 @@ import Button from '../components/common/Button'
 function RegisterLogin() {
   return (
     <div>
-      <a href="http://localhost:4000/auth/facebook">
+      <a href="/auth/facebook">
         <Button
           buttonText="Login with Facebook"
           classNames="social-button"
         />
       </a>
 
-      <a href="http://localhost:4000/auth/google">
+      <a href="/auth/google">
         <Button
           buttonText="Login with Google"
           classNames="social-button"
