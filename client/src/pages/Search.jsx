9c2d43c1c33c3a53031d5911ff527034c472d8d6
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-11 20:27:44 +0000

    Enhanced and simplified search by not floating objects over image, instead used background image property in CSS

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,23 +1,18 @@
 import React, { useContext } from 'react';
-import './Search.css'
-import Image from '../components/common/Image';
-import Search from '../components/search/Search'
+import './Search.css';
+import Search from '../components/search/Search';
 import { ViewContext } from '../context/ViewContext';
 
 function HomePage() {
   const size = useContext(ViewContext);
   return (
-    <div className={
-      size.device === 'Desktop'
-        ? 'home-top-container'
-        : 'home-top-container-mobile home-top-container'
-    }>
-      <Image src="/images/home-hero.png" alt="Laptop and plant hero" />
-      <div className="overlay-image-container">
+    <div
+      className={`image-container image-container-${size.device.toLowerCase()}`}
+    >
+      <div className="search-container">
         <h1>Search for your skillset</h1>
         <Search />
       </div>
-      
     </div>
   );
 }
