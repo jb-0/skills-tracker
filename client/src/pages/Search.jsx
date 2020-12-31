de6aeb4967d21e526d58bbf29cb458ba4e40373e
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-12-31 12:15:45 +0000

    Provided explanatory text for usage of the search form

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -11,6 +11,8 @@ function HomePage() {
     >
       <div className="search-container">
         <h1>Search for your skillset</h1>
+        <p>Select a location and begin typing a skill, click a skill from the predefined list to add
+        it to your search. Finally click the search button to start searching. </p>
         <Search />
       </div>
     </div>
