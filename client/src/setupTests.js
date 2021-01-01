c931c97d758e3f1e515f8c8b382d2f34f2f859f9
Jamie Barrett <timothybarrett@hotmail.co.uk>
2021-01-01 15:58:02 +0000

    Added canvas mocks

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,6 +1,7 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import fetchMock from 'jest-fetch-mock';
+import 'jest-canvas-mock';
 
 Enzyme.configure({ adapter: new Adapter() })
 
