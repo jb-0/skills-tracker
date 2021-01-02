b40f7ab6d370d7fe792cd7e7ae20aa5f379c0d76
Jamie Barrett <timothybarrett@hotmail.co.uk>
2021-01-02 12:08:03 +0000

    Added jesst canvas mock and provided a simple mock for chart.js to enable testing

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,7 +1,10 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import fetchMock from 'jest-fetch-mock';
+import 'jest-canvas-mock';
 
 Enzyme.configure({ adapter: new Adapter() })
 
 fetchMock.enableMocks();
+
+jest.mock('chart.js', () => jest.fn().mockImplementation(() => null));
