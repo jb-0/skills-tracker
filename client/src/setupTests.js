814fbfcaf7a49812873267cccbbf0c25fbdedcd4
Jamie Barrett <timothybarrett@hotmail.co.uk>
2021-01-02 10:35:42 +0000

    Dropped testing for profile container for now. Moved to individual ticket.

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,7 +1,6 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import fetchMock from 'jest-fetch-mock';
-import 'jest-canvas-mock';
 
 Enzyme.configure({ adapter: new Adapter() })
 
