8eb35b533a2e750479a002eb73890d1925018af3
Jamie Barrett <timothybarrett@hotmail.co.uk>
2021-01-24 13:20:42 +0000

    Migrated profile container styles to styled component

---
+++ client/src/components/profile/ProfileContainer.Styles.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const SavedSearchesContainer = styled.div`
+  color: var(--secondary-color);
+  width: 90%;
+  margin: auto;
+  
+  & p {
+    color: var(--secondary-color);
+  }
+`;
+
+export const SavedSearchCardsGrid = styled.section`
+  padding: 30px 0;
+  margin: auto;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+  grid-gap: 1rem;
+`;
