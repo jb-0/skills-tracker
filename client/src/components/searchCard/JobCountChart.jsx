543b8401b5054bab92742d69379de101461c4b20
Jamie Barrett <timothybarrett@hotmail.co.uk>
2021-01-24 14:14:31 +0000

    Reduced border width on chart

--- client/src/components/searchCard/JobCountChart.jsx
+++ client/src/components/searchCard/JobCountChart.jsx
@@ -15,6 +15,7 @@ function JobCountChart(props) {
               label: 'Number of jobs',
               backgroundColor: '#ffb703',
               borderColor: '#212529',
+              borderWidth: '1.5',
               data: props.search.dailySearchTermCount.map((day) => day.count),
             },
           ],
