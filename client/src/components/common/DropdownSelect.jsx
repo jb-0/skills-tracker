9e449b3f425aa6a1af923342f340b0b0085e6807
Jamie Barrett <timothybarrett@hotmail.co.uk>
2021-01-26 07:07:16 +0000

    Migrated dropdown select styles to styled component

--- client/src/components/common/DropdownSelect.jsx
+++ client/src/components/common/DropdownSelect.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import './DropdownSelect.css'
+import { DropdownSelectStyled } from './DropdownSelect.Styles';
 
 function DropdownSelect(props) {
   return (
-    <div className="dropdown-select">
-      <select name={props.name} id={props.name} value={props.value} onChange={props.onChange}>
-        {props.selectOptions.map((selection) => {
-          return <option key={uuidv4()} value={selection}>{selection}</option>;
-        })}
-      </select>
-    </div>
+    <DropdownSelectStyled name={props.name} id={props.name} value={props.value} onChange={props.onChange}>
+      {props.selectOptions.map((selection) => {
+        return <option key={uuidv4()} value={selection}>{selection}</option>;
+      })}
+    </DropdownSelectStyled>
   );
 }
 
