60243b7126b9fb0bdc225a6c320f4670fdbe4fe0
Jamie Barrett <timothybarrett@hotmail.co.uk>
2021-02-04 07:32:58 +0000

    Mocked firebase firestore in setup tests

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,10 +1,41 @@
-import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import fetchMock from 'jest-fetch-mock';
 import 'jest-canvas-mock';
 
-Enzyme.configure({ adapter: new Adapter() })
+Enzyme.configure({ adapter: new Adapter() });
 
 fetchMock.enableMocks();
 
 jest.mock('chart.js', () => jest.fn().mockImplementation(() => null));
+
+jest.mock('firebase/app', () => ({
+  __esModule: true,
+  default: {
+    apps: [],
+    initializeApp: () => {},
+    auth: () => {},
+    firestore: () => {
+      return {
+        collection: () => {
+          return {
+            get: () => {
+              return [
+                {
+                  data: () => {
+                    return { locations: ['Coventry', 'Essex', 'London'] };
+                  },
+                },
+                {
+                  data: () => {
+                    return { skills: ['Node'] };
+                  },
+                },
+              ];
+            },
+          };
+        },
+      };
+    },
+  },
+}));
