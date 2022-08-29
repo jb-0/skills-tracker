22d6123fc9f8998ab250762f24813334815eb370
Jamie Barrett <timothybarrett@hotmail.co.uk>
2022-08-29 12:19:34 +0100

    chore: fix react prod err

--- client/src/components/AppBar/index.tsx
+++ client/src/components/AppBar/index.tsx
@@ -20,12 +20,6 @@ import LogoImg from '../../images/logo.svg';
 const pages = ['Home', 'Search'];
 const settings = ['Profile', 'Logout'];
 
-const logo = (
-  <Icon sx={{ mr: 1 }}>
-    <img src={LogoImg} height={25} width={25} alt="" />
-  </Icon>
-);
-
 const ResponsiveAppBar = () => {
   const [userState] = useContext(UserContext);
   if (!userState.authenticated && !pages.includes('Login')) pages.push('Login');
@@ -52,111 +46,115 @@ const ResponsiveAppBar = () => {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <>
-            {logo}
-            <Box sx={{ flexGrow: 1, display: { xs: 'flex', sm: 'none' } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
+          <Icon sx={{ mr: 1 }}>
+            <img src={LogoImg} height={25} width={25} alt="" />
+          </Icon>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', sm: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', sm: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem
+                  key={page}
+                  component={Button}
+                  onClick={handleCloseNavMenu}
+                  href={page !== 'Home' ? `/${page.toLowerCase()}` : '/'}
+                >
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          {!userState.authenticated ? (
+            <Icon sx={{ mr: 1 }}>
+              <img src={LogoImg} height={25} width={25} alt="" />
+            </Icon>
+          ) : null}
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', sm: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                href={page !== 'Home' ? `/${page.toLowerCase()}` : '/'}
+                sx={{ my: 2, color: 'white', display: 'block' }}
               >
-                <MenuIcon />
-              </IconButton>
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          {userState.authenticated ? (
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar />
+                </IconButton>
+              </Tooltip>
               <Menu
+                sx={{ mt: '45px' }}
                 id="menu-appbar"
-                anchorEl={anchorElNav}
+                anchorEl={anchorElUser}
                 anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'left',
+                  vertical: 'top',
+                  horizontal: 'right',
                 }}
                 keepMounted
                 transformOrigin={{
                   vertical: 'top',
-                  horizontal: 'left',
+                  horizontal: 'right',
                 }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: 'block', sm: 'none' },
-                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
               >
-                {pages.map((page) => (
+                {settings.map((setting) => (
                   <MenuItem
-                    key={page}
+                    key={setting}
                     component={Button}
-                    onClick={handleCloseNavMenu}
-                    href={page !== 'Home' ? `/${page.toLowerCase()}` : '/'}
+                    {...(setting.toLowerCase() === 'logout'
+                      ? {
+                          onClick: () => {
+                            fetch('/api/user/logout');
+                            handleCloseUserMenu();
+                          },
+                          href: '/',
+                        }
+                      : {
+                          href: `/${setting.toLowerCase()}`,
+                          onClick: handleCloseUserMenu,
+                        })}
                   >
-                    <Typography textAlign="center">{page}</Typography>
+                    <Typography textAlign="center">{setting}</Typography>
                   </MenuItem>
                 ))}
               </Menu>
             </Box>
-
-            {!userState.authenticated ? { logo } : null}
-            <Box sx={{ flexGrow: 1, display: { xs: 'none', sm: 'flex' } }}>
-              {pages.map((page) => (
-                <Button
-                  key={page}
-                  onClick={handleCloseNavMenu}
-                  href={page !== 'Home' ? `/${page.toLowerCase()}` : '/'}
-                  sx={{ my: 2, color: 'white', display: 'block' }}
-                >
-                  {page}
-                </Button>
-              ))}
-            </Box>
-
-            {userState.authenticated ? (
-              <Box sx={{ flexGrow: 0 }}>
-                <Tooltip title="Open settings">
-                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                    <Avatar />
-                  </IconButton>
-                </Tooltip>
-                <Menu
-                  sx={{ mt: '45px' }}
-                  id="menu-appbar"
-                  anchorEl={anchorElUser}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  open={Boolean(anchorElUser)}
-                  onClose={handleCloseUserMenu}
-                >
-                  {settings.map((setting) => (
-                    <MenuItem
-                      key={setting}
-                      component={Button}
-                      {...(setting.toLowerCase() === 'logout'
-                        ? {
-                            onClick: () => {
-                              fetch('/api/user/logout');
-                              handleCloseUserMenu();
-                            },
-                            href: '/',
-                          }
-                        : {
-                            href: `/${setting.toLowerCase()}`,
-                            onClick: handleCloseUserMenu,
-                          })}
-                    >
-                      <Typography textAlign="center">{setting}</Typography>
-                    </MenuItem>
-                  ))}
-                </Menu>
-              </Box>
-            ) : null}
-          </>
+          ) : null}
         </Toolbar>
       </Container>
     </AppBar>
